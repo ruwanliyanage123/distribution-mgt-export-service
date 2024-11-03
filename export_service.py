5dd35367f2a5013ce6a5d5dedd40c5b524999d86
Ruwan <ruwanliyanagegalle1994@gmail.com>
2024-11-03 12:44:57 +0530

    Add export service

---
+++ export_service.py
@@ -0,0 +1,17 @@
+# Example usage
+import shutil
+
+from config.config import Config
+from service.s3_service import S3Service
+
+if __name__ == "__main__":
+    bucket_name = Config.S3_BUCKET_NAME
+    object_keys = ['in-progress/in_progress_apple.jpeg']
+    download_path = 'images'
+    upload_s3_key = 'exported'
+    prefix = 'approved'
+    s3_service = S3Service()
+    s3_service.download_s3_objects(bucket_name, object_keys, download_path)
+    renamed_files = s3_service.rename_files(download_path, prefix)
+    s3_service.upload_files(bucket_name, download_path, upload_s3_key)
+    shutil.rmtree(download_path)
